bb21d8b1ccb6f835137258f393e3f05ab076cb87
KranzAklilu <64352846+KranzAklilu@users.noreply.github.com>
2022-10-13 09:51:47 +0300

    Time: 1316 ms (5.86%), Space: 52.3 MB (5.86%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.ts
@@ -0,0 +1,15 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function sortColors(nums: number[]): void {
+    for(let i = 0; i < nums.length; i++){
+        for(let j = 0; j < nums.length - i; j++){
+            if(nums[j] > nums[j + 1]){
+            const temp = nums[j]
+            nums[j] = nums[j + 1]
+            nums[j + 1] = temp
+                console.log(nums)
+            }
+        }
+    }
+};
